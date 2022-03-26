19bc3ffc9d4a9c0ad516ea8ccc16abdb21f7ca3f
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-03-26 20:41:51 +0530

    Recursion

---
+++ DSA/Recursion/PrintNumber.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void fun(int n)
+{
+    if(n > 0)
+    {
+      fun(n-1);
+      cout<<n;
+    }
+    else
+    {
+        cout<<"0";
+    }
+    
+}
+int main()
+{
+    int n;
+    cout << "Enter the number: " <<endl;
+    cin >> n; 
+    fun(n);
+}
