01a986c6a0cd28147e9df361e094cd11d622ca43
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-03-26 21:33:08 +0530

    SumOfNumber

--- DSA/Recursion/PrintNumber.cpp
+++ DSA/Recursion/PrintNumber.cpp
@@ -4,12 +4,13 @@ void fun(int n)
 {
     if(n > 0)
     {
+    //   fun(n-1);
+      cout<<" "<<n;
       fun(n-1);
-      cout<<n;
     }
     else
     {
-        cout<<"0";
+        cout<<" 0";
     }
     
 }
