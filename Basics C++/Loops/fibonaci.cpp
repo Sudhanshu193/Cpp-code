16aa54959e8841b90857f66b67f9917248c1d483
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-03-29 00:44:36 +0530

    Modified

--- Basics C++/Loops/fibonaci.cpp
+++ Basics C++/Loops/fibonaci.cpp
@@ -5,7 +5,7 @@ int main()
 {
     int n, n1 = 0, n2 = 1, n3;
     cout<<"Enter the number"<<endl;
-    cin>>n;
+    cin>> n;
     cout << n1 << " " << n2 << " ";
     for (int i = 0; i < n; i++)
     {
