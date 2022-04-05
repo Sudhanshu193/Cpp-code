70ca9e208e56ed4c82a11db861156e513283399d
Sudhanshu Tripathi <sudhanshutripathi0888@gmail.com>
2022-04-05 20:37:28 +0530

    modification

--- Basics C++/Pointers/adding_matrix_using_pointer.cpp
+++ Basics C++/Pointers/adding_matrix_using_pointer.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 int main()
 {
-    int a[3][3],b[3][3],c[3][3],i,j;
+    int b[3][3],c[3][3],i,j;
 
+    
+    for(i = 0; i < 3; i++)
+    {
+      for(j = 0; j < 3; j++)
+          cout << *(*(a + i) + j) << " ";
+      cout << endl;
+    }
+
+    int A[10] = {12,23};
+    cout << *(A + 1) << endl;
 
     cout << "Enter matrix 1 element"<<endl;
     for (i = 0; i < 3; i++)
@@ -20,7 +30,7 @@ int main()
         for ( j = 0; j < 3; j++)
         {
           cin >>*(*(b+i)+j);
-        }  
+      }       
         cout<<endl; 
     }
     for ( i = 0; i < 3; i++)
